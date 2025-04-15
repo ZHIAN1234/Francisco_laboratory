980520dbea7cdcf881e655f712aa074cb6f8c632
ZHIAN1234 <zhianfrancisco257@gmail.com>
2025-04-15 16:43:24 +0800

    update

---
+++ Activity3/Person.java
@@ -0,0 +1,45 @@
+package Activity3;
+
+import java.util.Scanner;
+
+public class Person {
+    private String name;
+    private int id;
+
+public Person (String name, int id) {
+    this.name = name;
+    this.id = id;
+
+}
+
+public void displayInfo() {
+    System.out.println("Name: " + name);
+    System.out.println("ID: " + id);
+
+    }
+}
+
+ class SchoolTest {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+
+        Student student = new Student("Ria", 2023, "3rd");
+        Teacher teacher = new Teacher("Jorgina", 143, "Integral Calculus");
+        Staff staff = new Staff("Jethro", 998, "IT" );
+    
+
+    System.out.println("Student Information: ");
+    student.displayInfo();
+    System.out.println();
+
+    System.out.println("Teacher: ");
+    teacher.displayInfo();
+    System.out.println();
+
+    System.out.println("Staff: ");
+    staff.displayInfo();
+    System.out.println();
+
+    }
+}
---
+++ Activity3/Staff.java
@@ -0,0 +1,17 @@
+package Activity3;
+
+    public class Staff extends Person {
+        private String department;
+    
+        public Staff (String name, int id, String department) {
+            super(name,id);
+            this.department = department;
+    
+        }
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Department: " + department);
+        }    
+    }
+
